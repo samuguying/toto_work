d43b19ebbf5da0cdcab8eded329b480854f3ad17
wt <39181819@qq.com>
2020-04-27 12:34:06 +0800

    [IMP] 加入一些模型、字段约束

--- work_plan/models/toto_class_type.py
+++ work_plan/models/toto_class_type.py
@@ -5,3 +5,7 @@ class ClassType(models.Model):
     _name = 'toto.work.class.type'
 
     name = fields.Char('名称', required=True)
+
+    _sql_constraints = [
+        ("name_uniq", "unique (name)", "名称不能重复！"),
+    ]
--- work_plan/models/toto_dissolve_device.py
+++ work_plan/models/toto_dissolve_device.py
@@ -5,3 +5,7 @@ class DissolveDevice(models.Model):
     _name = 'toto.dissolve.device'
 
     name = fields.Char('设备名称', required=True)
+
+    _sql_constraints = [
+        ("name_uniq", "unique (name)", "名称不能重复！"),
+    ]
