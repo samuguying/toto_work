3154eabf6cc4bcbc94ad03706231ae50c00ffacc
kylinxjd <762741385@qq.com>
2020-04-28 15:23:12 +0800

    添加不良记录等

--- work_plan/models/toto_dissolve_plan.py
+++ work_plan/models/toto_dissolve_plan.py
@@ -9,19 +9,22 @@ class DissolvePlan(models.Model):
     line_id = fields.Char('线别')
     staffing = fields.Integer('人员配置')
     vacation = fields.Char('休假', default='无')
+    item_ids = fields.One2many('toto.dissolve.plan.item', 'plan_id', '人员安排')
 
-    class DissolvePlanItem(models.Model):
-        _name = 'toto.dissolve.plan.item'
 
-        sequence = fields.Integer(string="Sequence", default=10)
-        plan_id = fields.Many2one('toto.dissolve.plan', '计划')
-        device_id = fields.Many2one('toto.dissolve.device', '设备')
-        dissovle_pinben= fields.Char('品番')
-        predetermined_quantity = fields.Integer('预定数量')
-        actual_quantity = fields.Integer('实际数量')
-        note = fields.Text('备注')
+class DissolvePlanItem(models.Model):
+    _name = 'toto.dissolve.plan.item'
 
-        display_type = fields.Selection([
-            ('line_section', "Section"),
-            ('line_note', "Note")], default=False, help="Technical field for UX purpose.")
-        name = fields.Char(string="名称")
+    sequence = fields.Integer(string="Sequence", default=10)
+    plan_id = fields.Many2one('toto.dissolve.plan', '计划')
+    user_id = fields.Many2one('res.users', '作业员', ondelete="restrict")
+    device_id = fields.Many2one('toto.dissolve.device', '设备')
+    dissolve_pinben = fields.Char('品番')
+    predetermined_quantity = fields.Integer('预定数量')
+    actual_quantity = fields.Integer('实际数量')
+    note = fields.Text('备注')
+
+    display_type = fields.Selection([
+        ('line_section', "Section"),
+        ('line_note', "Note")], default=False, help="Technical field for UX purpose.")
+    name = fields.Char(string="名称")
