1f8574c9e981026723b53229b1192694a526244e
kylinxjd <762741385@qq.com>
2020-04-29 14:54:20 +0800

    添加日历视图

--- work_plan/models/toto_work_plan.py
+++ work_plan/models/toto_work_plan.py
@@ -4,23 +4,30 @@ from odoo import models, fields, api
 class WorkPlan(models.Model):
     _inherit = ["mail.thread", 'mail.activity.mixin']
     _name = 'toto.work.plan'
+    _rec_name = 'date'
 
     date = fields.Datetime('日期', default=fields.Datetime.now)
     class_id = fields.Many2one('toto.work.class.type', '班别', ondelete="restrict")
-    work_type = fields.Selection([ ('day', '早班'),('middle', '中班'),('night', '夜班')],string="班次")
+    work_type = fields.Selection([('day', '早班'), ('middle', '中班'), ('night', '夜班')], string="班次")
     vacation = fields.Char('休假', default='无')
-    staffing = fields.Integer('人员配置')
+    staffing = fields.Integer('出勤人数')
     bad_content = fields.Text('不良内容')
     user_ids = fields.Many2many('res.users', string='作业员', ondelete="restrict")
     detail_countermeasure = fields.Html('具体对策')
     item_ids = fields.One2many('toto.work.plan.item', 'plan_id', '人员安排')
+    plan_type = fields.Selection([
+        ('shaping', '成形系'),
+        ('power', '动力系'),
+        ('dissolve', '溶着系'),
+        ('process', '加工系'),
+    ])
 
 
 class WorkPlanItem(models.Model):
     _name = 'toto.work.plan.item'
 
     sequence = fields.Integer(string="Sequence", default=10)
-    plan_id = fields.Many2one('toto.work.plan', '计划', ondelete="cascade")
+    plan_id = fields.Many2one('toto.work.plan', '计划')
     device_id = fields.Many2one('toto.work.device', '设备', ondelete="restrict")
     user_id = fields.Many2one('res.users', '作业员', ondelete="restrict")
     work_subject = fields.Char('作业项目')
