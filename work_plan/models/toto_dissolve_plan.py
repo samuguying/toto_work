bf1d3fe2b427f784cf298006ff8a703679aa3d89
kylinxjd <762741385@qq.com>
2020-05-08 12:11:08 +0800

    添加hr部门

--- work_plan/models/toto_dissolve_plan.py
+++ work_plan/models/toto_dissolve_plan.py
@@ -6,12 +6,12 @@ class WorkPlanDissolve(models.Model):
     _name = 'toto.dissolve.plan'
 
     line_id = fields.Many2one('toto.work.line', '线别')
-    item_ids = fields.One2many('toto.dissolve.plan.item', 'dissolve_plan_id', string='人员安排')
+    item_ids = fields.One2many('toto.dissolve.plan.item', 'dissolve_plan_id', string='人员安排', ondelete="cascade")
 
 
-class WorkPlanPowerItem(models.Model):
+class WorkPlanDissolveItem(models.Model):
     _inherit = 'toto.work.plan.item'
     _name = 'toto.dissolve.plan.item'
 
-    product_id = fields.Many2one('toto.work.plan.product', '作业内容', ondelete="restrict")
-    dissolve_plan_id = fields.Many2one('toto.dissolve.plan', '动力系计划')
+    product_id = fields.Many2one('toto.work.plan.product', '作业内容(品番)', ondelete="restrict")
+    dissolve_plan_id = fields.Many2one('toto.dissolve.plan', '溶着系计划')
