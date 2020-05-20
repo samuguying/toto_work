b9a6df17596282b127753a86a3e11c0e48d512f6
kylinxjd <762741385@qq.com>
2020-05-20 16:04:52 +0800

    溶着系修改，员工经理改为所属长

--- work_plan/models/toto_dissolve_plan.py
+++ work_plan/models/toto_dissolve_plan.py
@@ -1,18 +1,28 @@
+from datetime import datetime
+
 from odoo import models, fields, api
 
 
 class WorkPlanDissolve(models.Model):
-    _inherit = 'toto.work.plan'
+    _inherit = ["mail.thread", "mail.activity.mixin"]
     _name = 'toto.dissolve.plan'
 
     assume_user_id = fields.Many2one('res.users', '担当', default=lambda self: self.env.user)
-    item_ids = fields.One2many('toto.dissolve.plan.item', 'dissolve_plan_id', string='人员安排', ondelete="cascade")
+    date = fields.Datetime('日期', default=fields.Datetime.now)
+    device_id = fields.Many2one('toto.work.device', '设备', ondelete="restrict", domain="[('device_type','=','dissolve')]")
+    staffing = fields.Integer('人员配置')
+    product_id = fields.Many2one('toto.work.plan.product', '作业内容(品番)', ondelete="restrict")
+    predetermined_quantity = fields.Integer('预定数量', default=None)
+    actual_quantity = fields.Integer('实际数量', default=None)
+    note = fields.Text('备注')
     state = fields.Selection([
         ('process', '未完成'),
         ('finished', '完成'),
     ], default='process', string='达成情况')
-    # employee_ids = fields.One2many('hr.employee', string="作业员",
-    #                                related='assume_user_id.employee_ids.child_ids')
+
+    @api.multi
+    def name_get(self):
+        return [(dissolve.id, "%s-%s" % (dissolve.device_id.name, datetime.strftime(dissolve.date, "%Y年%m月%d日"))) for dissolve in self]
 
     def action_finish(self):
         self.ensure_one()
@@ -21,12 +31,3 @@ class WorkPlanDissolve(models.Model):
     def action_renew(self):
         self.ensure_one()
         self.state = 'process'
-
-
-class WorkPlanDissolveItem(models.Model):
-    _inherit = 'toto.work.plan.item'
-    _name = 'toto.dissolve.plan.item'
-
-    product_id = fields.Many2one('toto.work.plan.product', '作业内容(品番)', ondelete="restrict")
-    dissolve_plan_id = fields.Many2one('toto.dissolve.plan', '溶着系计划')
-    operator = fields.Many2one('res.users', '作业员',)
