ad44b0307666a57d64e4e1a52b7ddac1df4ef76b
samuguying <50394056+samuguying@users.noreply.github.com>
2020-06-01 14:09:13 +0800

    Revert "成形系修复选作业员只选本班的  溶着系模型改为继承toto.work.plan  默认班别代码修改。溶着系添加状态"

--- work_plan/models/toto_work_plan.py
+++ work_plan/models/toto_work_plan.py
@@ -6,17 +6,10 @@ class WorkPlan(models.Model):
     _name = 'toto.work.plan'
     _rec_name = 'date'
 
-    def _default_class_type_id(self):
-        depts = self.env.user.employee_ids.mapped('department_id')
-        if depts:
-            return depts[0]
-        else:
-            return None
-
     date = fields.Datetime('日期', default=fields.Datetime.now)
     # class_id = fields.Many2one('toto.work.class.type', '班别', ondelete="restrict")
     class_type_id = fields.Many2one('hr.department', '班别', ondelete="restrict",
-                                    default=_default_class_type_id,
+                                    default=lambda self: self.env.user.employee_ids.mapped('department_id'),
                                     domain=lambda self: [('id', 'in', self.env.user.employee_ids.mapped('department_id').ids)])
     work_type = fields.Selection([('day', '早班'), ('middle', '中班'), ('night', '夜班')], string="班次")
     vacation = fields.Char('休假', default='无')
@@ -34,6 +27,7 @@ class WorkPlanItem(models.Model):
     _name = 'toto.work.plan.item'
 
     sequence = fields.Integer(string="Sequence", default=10)
+    plan_id = fields.Many2one('toto.work.plan', '计划')
     device_id = fields.Many2one('toto.work.device', '设备', ondelete="restrict")
     employee_id = fields.Many2one('hr.employee', '作业员', ondelete="restrict")
     # domain="[('department_id', '=', parent.class_type_id)]"
