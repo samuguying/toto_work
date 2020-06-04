64628371399c0922dc3a6e606d3587ef9a068277
kylinxjd <762741385@qq.com>
2020-06-04 15:22:08 +0800

    动力系界面调整

--- work_plan/models/toto_power_plan.py
+++ work_plan/models/toto_power_plan.py
@@ -5,7 +5,46 @@ class WorkPowerPlan(models.Model):
     _inherit = "toto.work.plan"
     _name = 'toto.power.plan'
 
-    item_ids = fields.One2many('toto.power.plan.item', 'power_plan_id', string='人员安排')
+    create_user_id = fields.Many2one("res.users", "创建人", default=lambda self: self.env.user, readonly=True)
+    project = fields.Char("作业项目", track_visibility='onchange', required=True)
+    content = fields.Char("作业内容", track_visibility='onchange', required=True)
+    user_id = fields.Many2one("res.users", string='作业者', ondelete="restrict",
+                              domain="[('id', 'in', users_id)]")
+    priority = fields.Selection([
+        ('0', '很低'),
+        ('1', '低'),
+        ('2', '中'),
+        ('3', '高'),
+    ], string="优先级", default='1', track_visibility='onchange')
+    plan_time = fields.Datetime("计划时间", track_visibility='onchange')
+    finish_time = fields.Datetime("完成时间", readonly=True, track_visibility='onchange')
+    duration = fields.Float("持续时间", digits=(3, 1))
+    note = fields.Text("备注")
+    item_ids = fields.One2many("toto.power.plan.item", "power_plan_id")
+    state = fields.Selection([
+        ('draft', '新的'),
+        ('process', '进行中'),
+        ('done', '完成'),
+        ('cancel', '结束'),
+    ], default="draft", string="状态", track_visibility='onchange')
+
+    def name_get(self):
+        return [(p.id, "%s" % (p.project, )) for p in self]
+
+    def action_start(self):
+        self.ensure_one()
+        self.state = "process"
+
+    def action_done(self):
+        self.ensure_one()
+        self.write({
+            'state': 'done',
+            'finish_time': fields.datetime.now()
+        })
+
+    def action_cancel(self):
+        self.ensure_one()
+        self.state = "cancel"
 
 
 class WorkPowerPlanItem(models.Model):
